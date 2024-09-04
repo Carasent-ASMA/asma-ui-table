377e3a5563449035c268c75a3ec572f3691dff55
Sebastian Darie <sebastian.darie@carasent.com>
2024-09-04 08:17:13 +0000

    Merged in fix/export-drag-handle (pull request #9)
    Fix/export drag handle
    
    * fix: Export row drag handle
    
    * 1.0.13

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './types'
 export * from './components/StyledTableIndex'
+export { RowDragHandleCell } from './components/columns/dndHandleColumn'
 
 export { createColumnHelper, type Table, type Row, type ColumnDef } from '@tanstack/react-table'
