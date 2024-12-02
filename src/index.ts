6683b44242f73f1bdb9fde9fc55b36941f374c55
Grini Andrei <andrei.grini@carasent.com>
2024-12-02 17:13:40 +0200

    v1.0.37

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export * from './types'
 export * from './components/StyledTableIndex'
 export { RowDragHandleCell } from './components/columns/dndHandleColumn'
 
-export { createColumnHelper, type Table, type Row, type ColumnDef } from '@tanstack/react-table'
+export { createColumnHelper, type Table, type Row, type ColumnDef, type CellContext } from '@tanstack/react-table'
