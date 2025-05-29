8f5213df2287a41f8bf05a53daba9c68842ef3cc
Edward Nikolaev <nikolaev.eduard.vasilievich@gmail.com>
2025-05-29 14:24:10 +0300

    fix: ASMA-5214 Adjusted suspense overlay state for asma table

--- src/components/TableSkeleton.tsx
+++ src/components/TableSkeleton.tsx
@@ -1,15 +1,20 @@
 import { Skeleton } from '@mui/material'
+import { Fragment, type FunctionComponent } from 'react'
 
-export const TableSkeleton: React.FC<{ colSpan: number }> = ({ colSpan }) => {
-    return (
-        <>
-            {Array.from({ length: 5 }).map((_, index) => (
-                <tr key={index}>
-                    <td colSpan={colSpan}>
-                        <Skeleton key={index} variant='text' width='100%' height={50} />
-                    </td>
-                </tr>
-            ))}
-        </>
-    )
-}
+export const TableSkeleton = (({ colSpan, rowHeight = 48 }) => (
+    <Fragment>
+        {Array.from({ length: 10 }).map((_, index) => (
+            <tr key={index}>
+                <td colSpan={colSpan} height={rowHeight} className={'last:pb-0 first:pt-2 pb-2'}>
+                    <Skeleton
+                        key={index}
+                        variant={'rectangular'}
+                        width={'100%'}
+                        height={40}
+                        sx={{ backgroundColor: 'var(--colors-delta-50)', borderRadius: '8px' }}
+                    />
+                </td>
+            </tr>
+        ))}
+    </Fragment>
+)) satisfies FunctionComponent<{ colSpan: number; rowHeight?: number }>
